4ab0575bf6c0b36dd13275101a7d0355eaea3483
Erika Pauwels <erika.pauwels@gmail.com>
2019-09-28 16:31:50 +0200

    Add body full height CSS class only in document-view route

--- app/routes/document-view.js
+++ app/routes/document-view.js
@@ -1,7 +1,16 @@
 import Route from '@ember/routing/route';
+import $ from 'jquery';
 
 export default Route.extend({
   async model(params) {
     return this.store.findRecord('document-version', params.id);
+  },
+
+  activate() {
+    $('body').addClass('vlc-u-box-model-maximize-height vlc-scroll-wrapper vl-u-no-overflow');
+  },
+
+  deactivate() {
+    $('body').removeClass('vlc-u-box-model-maximize-height vlc-scroll-wrapper vl-u-no-overflow');
   }
 });
