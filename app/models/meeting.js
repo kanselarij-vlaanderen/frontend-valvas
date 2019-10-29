092f6e40e6e0d9b5d985b0eb26acc2ba2f0cc6a8
Erika Pauwels <erika.pauwels@gmail.com>
2019-10-29 11:04:49 +0100

    Add electronic meeting flag to session title in search results

--- app/models/meeting.js
+++ app/models/meeting.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 let { Model, attr, hasMany } = DS;
+import { equal } from '@ember/object/computed';
 
 export default Model.extend({
   plannedStart: attr('date'),
@@ -9,7 +10,9 @@ export default Model.extend({
   number: attr('number'),
   isFinal: attr('boolean'),
   extraInfo: attr('string'),
-  kind: attr('string'),
+  type: attr('string'),
 
-  notifications: hasMany('notification')
+  notifications: hasMany('notification'),
+
+  isElectronic: equal('type', 'http://kanselarij.vo.data.gift/id/concept/ministerraad-type-codes/406F2ECA-524D-47DC-B889-651893135456')
 });
