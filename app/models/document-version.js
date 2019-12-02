f738d752b7e5fccfe3f145ce5e720e5da6edbeaf
Erika Pauwels <erika.pauwels@gmail.com>
2019-12-02 13:45:11 +0100

    Display document title instead of file title

--- app/models/document-version.js
+++ app/models/document-version.js
@@ -3,6 +3,7 @@ const { Model, hasMany, belongsTo } = DS;
 
 export default Model.extend({
   newsItems: hasMany('newsletter-info', { inverse: null }),
+  document: belongsTo('document', { inverse: null }),
   file: belongsTo('file', { inverse: null }),
   convertedFile: belongsTo('file', { inverse: null }),
 });
