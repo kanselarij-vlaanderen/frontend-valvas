3350c9458da71544fd8af570432a91e4315d7b42
Michaël Dierick <michael.dierick@redpencil.io>
2020-01-17 17:24:33 +0100

    Forgotten changes in ember-data model

--- app/models/document.js
+++ app/models/document.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
-const { Model, hasMany, belongsTo } = DS;
+const { Model, hasMany, belongsTo, attr } = DS;
 
 export default Model.extend({
+  title: attr('string'),
   newsItems: hasMany('newsletter-info', { inverse: null }),
   document: belongsTo('document-container', { inverse: null }),
   file: belongsTo('file', { inverse: null }),
--- app/models/newsletter-info.js
+++ app/models/newsletter-info.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 const { Model, hasMany } = DS;
 
 export default Model.extend({
-  documentVersions: hasMany('document-version', { inverse: null })
+  documents: hasMany('document', { inverse: null })
 });
