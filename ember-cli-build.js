c84b1f6da15bebc297067613287d07be6579d719
Erika Pauwels <erika.pauwels@gmail.com>
2020-05-14 17:03:55 +0200

    Include url-polyfill for IE11

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -25,6 +25,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('node_modules/url-polyfill/url-polyfill.min.js');
 
   return app.toTree();
 };
