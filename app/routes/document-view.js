532d0ee610395e789f9f4b290006acf97755c37b
Flor Sanders <me@florsanders.be>
2021-04-24 10:25:09 +0200

    [chore] Rewrote routes in Octane syntax

--- app/routes/document-view.js
+++ app/routes/document-view.js
@@ -1,16 +1,16 @@
 import Route from '@ember/routing/route';
 import $ from 'jquery';
 
-export default Route.extend({
+export default class DocumentViewRoute extends Route {
   async model(params) {
-    return this.store.findRecord('document-version', params.id);
-  },
+    return this.store.findRecord('file', params.id);
+  }
 
   activate() {
     $('body').addClass('vlc-u-box-model-maximize-height vlc-scroll-wrapper vl-u-no-overflow');
-  },
+  }
 
   deactivate() {
     $('body').removeClass('vlc-u-box-model-maximize-height vlc-scroll-wrapper vl-u-no-overflow');
   }
-});
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,11 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default Route.extend({
-  store: service(),
-  searchNewsItems: service(),
+export default class IndexRoute extends Route {
+  @service store;
+  @service searchNewsItems;
 
   model(params) {
     return this.searchNewsItems.search(params);
   }
-});
+}
