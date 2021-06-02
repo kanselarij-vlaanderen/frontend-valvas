9181d71b0b04ea6f2ffd06822840c566f4ebdc7b
Flor Sanders <me@florsanders.be>
2021-06-02 11:06:03 +0200

    [fix] Change sign for when to show notification

--- app/components/meeting-notifications.js
+++ app/components/meeting-notifications.js
@@ -3,6 +3,6 @@ import Component from '@glimmer/component';
 export default class MeetingNotificationsComponent extends Component {
   get showNotification() {
     // Meeting record is still a promise: use getter function
-    return this.args.meeting.get('plannedPublicationDate') <= Date.now();
+    return this.args.meeting.get('plannedPublicationDate') > Date.now();
   }
 }
