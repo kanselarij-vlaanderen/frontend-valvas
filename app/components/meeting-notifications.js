6469ca4d9bcff2fe10191191e4120cf67c24b569
Flor Sanders <me@florsanders.be>
2021-06-02 11:04:30 +0200

    [chore] Simplified meetingNotification component by passing meeting record (promise)

--- app/components/meeting-notifications.js
+++ app/components/meeting-notifications.js
@@ -1,20 +1,8 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 
 export default class MeetingNotificationsComponent extends Component {
-  @service store;
-
-  @tracked meeting = null;
-
-  constructor() {
-    super(...arguments);
-    this.meeting = this.store.findRecord('meeting', this.args.meetingId);
-  }
-
   get showNotification() {
-    return this.meeting
-      ? this.meeting.get('plannedPublicationDate') > Date.now()
-      : false;
+    // Meeting record is still a promise: use getter function
+    return this.args.meeting.get('plannedPublicationDate') <= Date.now();
   }
 }
