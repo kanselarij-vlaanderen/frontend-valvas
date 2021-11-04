ed38b3e810e6e5b65e79f6794419b98e8ca5d262
Michaël Dierick <michael.dierick@redpencil.io>
2021-11-04 16:22:48 +0100

    do away with hacks related to inconsistent legacy-data

--- app/components/format-ministers.js
+++ app/components/format-ministers.js
@@ -4,9 +4,7 @@ export default class FormatMinistersComponent extends Component {
   get sortedMandatees() {
     if (Array.isArray(this.args.mandatees)) {
       return this.args.mandatees.sort((a, b) => {
-        let pos_a = a.position || a.legacyPosition;
-        let pos_b = b.position || b.legacyPosition;
-        return pos_a - pos_b;
+        return a.position - b.position;
       });
     } else {
       return [];
--- app/models/mandatee.js
+++ app/models/mandatee.js
@@ -1,6 +1,5 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 import mandateTypeFromTitle from 'frontend-valvas/utils/mandate-type-from-title';
-import priorityFromMandateType from 'frontend-valvas/utils/priority-from-mandate-type';
 
 export default class MandateeModel extends Model {
   @attr('number') position;
@@ -12,12 +11,4 @@ export default class MandateeModel extends Model {
   @belongsTo('person') person;
   @belongsTo('mandate') mandate;
   @belongsTo('government-body') governmentBody;
-
-  get legacyTitle() {
-    return mandateTypeFromTitle(this.title);
-  }
-
-  get legacyPosition() {
-    return priorityFromMandateType(this.legacyTitle);
-  }
 }
