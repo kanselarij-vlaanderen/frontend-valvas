ff7fd0389e2606336cb25a041a6477e1034b6dc6
Erika Pauwels <erika.pauwels@gmail.com>
2022-04-14 14:59:43 +0200

    Refactor to logical check on planned publication date

--- app/components/meeting-notifications.js
+++ app/components/meeting-notifications.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 
 export default class MeetingNotificationsComponent extends Component {
   get showNotification() {
-    // Meeting record is still a promise: use getter function
-    return this.args.meeting.get('plannedPublicationDate') > Date.now();
+    return this.args.meeting.plannedPublicationDate > Date.now();
   }
 }
